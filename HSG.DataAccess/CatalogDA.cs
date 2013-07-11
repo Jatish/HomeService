e0f460f26f7694ff74a9276b8f15f2abf3226914
Jatish <jatish.shasamal@outlook.com>
2013-07-11 12:56:14 -0700

    ADD File

---
+++ HSG.DataAccess/CatalogDA.cs
@@ -0,0 +1,26 @@
+﻿using log4net;
+using System.Data;
+using HSG.DataAccess.Base;
+using System.Data.SqlClient;
+
+namespace HSG.DataAccess
+{
+    public class CatalogDA
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(CatalogDA));
+
+        /// <summary>
+        /// This method is used to get all the products from the catalog.
+        /// </summary>
+        /// <returns></returns>
+        public DataSet GetAllProducts()
+        {
+            logger.Debug("Method GetAllProducts called.");
+            SqlCommand objSqlCommand = new SqlCommand();
+            objSqlCommand.CommandText = "hsgGetAllProducts";
+            objSqlCommand.CommandType = CommandType.StoredProcedure;
+
+            return DBBase.Execute.ExecuteDataset(objSqlCommand);
+        }
+    }
+}
