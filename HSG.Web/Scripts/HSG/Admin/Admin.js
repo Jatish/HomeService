fb382594296c98adf321d3c5bc170ea80f600ebc
smrutihs <smruti.work@outlook.com>
2013-07-13 21:23:20 -0700

    added method to populate first page for admin users

--- HSG.Web/Scripts/HSG/Admin/Admin.js
+++ HSG.Web/Scripts/HSG/Admin/Admin.js
@@ -1,1 +1,47 @@
-﻿
+﻿/*
+*This function display class room Types to UI.
+*/
+function buildAdminMainPage() {
+    var sHtml = '';
+    sHtml += '<ul class="admin-main-menu-1 icons-menu-1a">';
+    sHtml += '<li onclick="">';
+    sHtml += '<div>';
+    sHtml += '<p>';
+    sHtml += '<h1>Catalog Manager</h1>';
+    sHtml += '</div>';
+    sHtml += '</li>';
+    sHtml += '<li onclick="">';
+    sHtml += '<div>';
+    sHtml += '<p>';
+    sHtml += '<h1>Order Manager</h1>';
+    sHtml += '</div>';
+    sHtml += '</li>';
+    sHtml += '<li onclick="">';
+    sHtml += '<div>';
+    sHtml += '<p>';
+    sHtml += '<h1>Inventory Manager</h1>';
+    sHtml += '</div>';
+    sHtml += '</li>';
+    sHtml += '</ul>';
+    sHtml += '<ul class="admin-main-menu-1 icons-menu-1a">';
+    sHtml += '<li onclick="">';
+    sHtml += '<div>';
+    sHtml += '<p>';
+    sHtml += '<h1>Marketing Manager</h1>';
+    sHtml += '</div>';
+    sHtml += '</li>';
+    sHtml += '<li onclick="">';
+    sHtml += '<div>';
+    sHtml += '<p>';
+    sHtml += '<h1>Coming Soon</h1>';
+    sHtml += '</div>';
+    sHtml += '</li>';
+    sHtml += '<li onclick="">';
+    sHtml += '<div>';
+    sHtml += '<p>';
+    sHtml += '<h1>Coming Soon</h1>';
+    sHtml += '</div>';
+    sHtml += '</li>';
+    sHtml += '</ul>';
+    $('#divAdminMainContent').html(sHtml);
+}
