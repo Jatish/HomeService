2c7485ec149795ac82c30956ded6fa9a66d1b6f3
smrutihs <smruti.work@outlook.com>
2013-07-15 01:53:23 -0700

    Added new method named as loadProductManagerContent' to load the ProductManager page content  to manage products.

--- HSG.Web/Scripts/HSG/Admin/Admin.js
+++ HSG.Web/Scripts/HSG/Admin/Admin.js
@@ -4,7 +4,7 @@
 function buildAdminMainPage() {
     var sHtml = '';
     sHtml += '<ul class="admin-main-menu-1 icons-menu-1a">';
-    sHtml += '<li onclick="">';
+    sHtml += '<li onclick="loadProductManagerContent();">';
     sHtml += '<div>';
     sHtml += '<p>';
     sHtml += '<h1>Catalog Manager</h1>';
@@ -40,6 +40,61 @@ function buildAdminMainPage() {
     sHtml += '<div>';
     sHtml += '<p>';
     sHtml += '<h1>Coming Soon</h1>';
+    sHtml += '</div>';
+    sHtml += '</li>';
+    sHtml += '</ul>';
+    $('#divAdminMainContent').html(sHtml);
+}
+
+/*
+ * This method is used to load ProductManager page content.
+ */
+function loadProductManagerContent() {
+    var sHtml = '';
+    sHtml += '<table width="100%"><tr>';
+    sHtml += '<td style="width:50%;"></td>';
+    sHtml += '<td style="text-align:right; vertical-align:middle;">';
+    sHtml += '<button id="btnAddNew" class="hsInputStyle" type="button" style="width:80px;"><span>Add New</span></button>&nbsp;';
+    sHtml += '<button id="btnDelectSelected" class="hsInputStyle" type="button" style="width:150px;"><span>Remove Selected</span></button>&nbsp;';
+    sHtml += '<button id="btnSelectAll" class="hsInputStyle" type="button" style="width:80px;"><span>Select All</span></button>&nbsp;';
+    sHtml += '</td></tr></table>';
+    sHtml += '<ul class="admin-main-menu-1 icons-menu-1a">';
+    sHtml += '<li onclick="">';
+    sHtml += '<div>';
+    sHtml += '<p>';
+    sHtml += '<h1><input type="checkbox" name="chkProduct" value="ProductID">Product 1</h1>';
+    sHtml += '</div>';
+    sHtml += '</li>';
+    sHtml += '<li onclick="">';
+    sHtml += '<div>';
+    sHtml += '<p>';
+    sHtml += '<h1><input type="checkbox" name="chkProduct" value="ProductID">Product 2</h1>';
+    sHtml += '</div>';
+    sHtml += '</li>';
+    sHtml += '<li onclick="">';
+    sHtml += '<div>';
+    sHtml += '<p>';
+    sHtml += '<h1><input type="checkbox" name="chkProduct" value="ProductID">Product 3</h1>';
+    sHtml += '</div>';
+    sHtml += '</li>';
+    sHtml += '</ul>';
+    sHtml += '<ul class="admin-main-menu-1 icons-menu-1a">';
+    sHtml += '<li onclick="">';
+    sHtml += '<div>';
+    sHtml += '<p>';
+    sHtml += '<h1><input type="checkbox" name="chkProduct" value="ProductID">Product 4</h1>';
+    sHtml += '</div>';
+    sHtml += '</li>';
+    sHtml += '<li onclick="">';
+    sHtml += '<div>';
+    sHtml += '<p>';
+    sHtml += '<h1><input type="checkbox" name="chkProduct" value="ProductID">Product 5</h1>';
+    sHtml += '</div>';
+    sHtml += '</li>';
+    sHtml += '<li onclick="">';
+    sHtml += '<div>';
+    sHtml += '<p>';
+    sHtml += '<h1><input type="checkbox" name="chkProduct" value="ProductID">Product 6</h1>';
     sHtml += '</div>';
     sHtml += '</li>';
     sHtml += '</ul>';
