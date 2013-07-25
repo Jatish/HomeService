27856df206be81338a40171bd746dc9bf57ffa92
smrutihs <smruti.work@outlook.com>
2013-07-25 01:42:56 -0700

    modified methods for getData and postData to take extra parameters as method reference which needs to be called depending upon the results returned from these ajax calls.

--- HSG.Web/Scripts/HSG/ServerInterface.js
+++ HSG.Web/Scripts/HSG/ServerInterface.js
@@ -7,37 +7,37 @@ arrParams = Parameters to be passed to the ajax call as object array.
 bIsCache = Should set cache ON as boolean value.
 bIsAsync = Is an aynchronous call as boolean value.
 bShowLoadPnl - Whether Page loading to be happen or not.
-func = Function reference to be called on success of the ajax cal.
+fnSuccess = Function reference to be called on success of the ajax cal.
+fnSuccessParams = Contains parameters to pass for the Success function.
 }
 */
-function getData(strUrl, arrParams, bIsCache, bIsAsync, bShowLoadPnl, func) {
+function getData(strUrl, arrParams, bIsCache, bIsAsync, bShowLoadPnl, fnSuccess, fnSuccessParams) {
     var objResponse = null;
     var bIsLoading = ((bShowLoadPnl != undefined) ? bShowLoadPnl : false);
     var bIsValid = checkSessionValidation();
     if (!bIsValid)
         window.location = jsonAppData.ContextPath + jsonAppData.DistrictShortName;
     else {
-    $.ajax({
-        cache: ((bIsCache == undefined) ? false : bIsCache),
-        type: 'GET',
-        async: ((bIsAsync == undefined) ? true : bIsAsync),
-        url: strUrl,
-        data: arrParams,
-        beforeSend: function () { if (bIsLoading) onBeginRequest(); },
-        dataType: 'json',
-        success: function (response) {
-            if (bIsLoading) onEndRequest();
-            if (response != undefined && response != '') {
-                if (func != undefined)
-                    func(response);
-                objResponse = response;
+        $.ajax({
+            cache: ((bIsCache == undefined) ? false : bIsCache),
+            type: 'GET',
+            async: ((bIsAsync == undefined) ? true : bIsAsync),
+            url: strUrl,
+            data: arrParams,
+            beforeSend: function () { if (bIsLoading) onBeginRequest(); },
+            dataType: 'json',
+            success: function (response) {
+                if (bIsLoading) onEndRequest();
+                if (response != undefined && response != '') {
+                    if (fnSuccess != undefined) fnSuccess(response, fnSuccessParams);
+                    objResponse = response;
+                }
+            },
+            error: function (message) {
+                if (bIsLoading) onEndRequest();
+                alert("Error occurred while processing your request. Please try again.");
             }
-        },
-        error: function (message) {
-            if (bIsLoading) onEndRequest();
-            alert("Error occurred while processing your request. Please try again.");
-        }
-    });
+        });
     }
     return objResponse;
 }
@@ -52,37 +52,38 @@ arrParams = Parameters to be passed to the ajax call as object array.
 bIsCache = Should set cache ON as boolean value.
 bIsAsync = Is an aynchronous call as boolean value.
 bShowLoadPnl - Whether Page loading to be happen or not.
-func = Function reference to be called on success of the ajax cal.
+fnSuccess = Function reference to be called on success of the ajax cal.
+fnSuccessParams = Contains parameters to pass for the Success function.
 }
 */
-function postData(strUrl, arrParams, bIsCache, bIsAsync, bShowLoadPnl, func) {
+function postData(strUrl, arrParams, bIsCache, bIsAsync, bShowLoadPnl, fnSuccess, fnSuccessParams) {
     var objResponse = null;
     var bIsLoading = ((bShowLoadPnl != undefined) ? bShowLoadPnl : false);
     var bIsValid = checkSessionValidation();
     if (!bIsValid)
         window.location = jsonAppData.ContextPath + jsonAppData.DistrictShortName;
     else {
-    $.ajax({
-        cache: ((bIsCache == undefined) ? false : bIsCache),
-        type: 'POST',
-        url: strUrl,
-        async: ((bIsAsync == undefined) ? true : bIsAsync),
-        dataType: 'json',
-        beforeSend: function () { if (bIsLoading) onBeginRequest(); },
-       //contentType: 'application/json; charset=utf-8',
-        data: arrParams,
-        success: function (response) {
-            if (bIsLoading) onEndRequest();
-            if (response != undefined && response != '') {
-                if (func != undefined) func(response);
-                objResponse = response;
+        $.ajax({
+            cache: ((bIsCache == undefined) ? false : bIsCache),
+            type: 'POST',
+            url: strUrl,
+            async: ((bIsAsync == undefined) ? true : bIsAsync),
+            dataType: 'json',
+            beforeSend: function () { if (bIsLoading) onBeginRequest(); },
+            //contentType: 'application/json; charset=utf-8',
+            data: arrParams,
+            success: function (response) {
+                if (bIsLoading) onEndRequest();
+                if (response != undefined && response != '') {
+                    if (fnSuccess != undefined) fnSuccess(response, fnSuccessParams);
+                    objResponse = response;
+                }
+            },
+            error: function (message) {
+                if (bIsLoading) onEndRequest();
+                alert("Error occurred while processing your request. Please try again.");
             }
-        },
-        error: function (message) {
-            if (bIsLoading) onEndRequest();
-            alert("Error occurred while processing your request. Please try again.");
-        }
-    });
+        });
     }
     return objResponse;
 }
