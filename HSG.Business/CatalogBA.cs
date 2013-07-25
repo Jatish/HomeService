8a5343788a0e306e4769bbc712e0d05c214faef4
smrutihs <smruti.work@outlook.com>
2013-07-25 01:37:42 -0700

    added new CatalogBA class with new methods for getting all products and saving a product in edit or new mode.

---
+++ HSG.Business/CatalogBA.cs
@@ -0,0 +1,39 @@
+﻿using log4net;
+using HSG.Library;
+using System.Data;
+using HSG.DataAccess;
+using System.Collections.Generic;
+
+namespace HSG.Business
+{
+    public class CatalogBA
+    {
+        private static readonly ILog logger = LogManager.GetLogger(typeof(CatalogBA));
+
+        /// <summary>
+        /// This method is used to get all the products and related data as a Dictionary object.
+        /// </summary>
+        /// <returns>Products and related data as Dictionary.</returns>
+        public Dictionary<string, object> GetProducts()
+        {
+            Dictionary<string, object> dicProducts = new Dictionary<string, object>();
+            DataSet dsProducts = new CatalogDA().GetAllProducts();
+            if (dsProducts.Tables.Count > 0)
+            {
+
+            }
+            return dicProducts;
+        }
+
+        /// <summary>
+        /// This method is used to save the product
+        /// </summary>
+        /// <param name="objProduct">Product information as ProductDO object.</param>
+        /// <returns>ProductID as integer.</returns>
+        public int SaveProduct(ProductDO objProduct)
+        {
+            logger.Debug("Method SaveProduct - objProduct.ProductID " + objProduct.ProductID);
+            return new CatalogDA().SaveProduct(objProduct);
+        }
+    }
+}
