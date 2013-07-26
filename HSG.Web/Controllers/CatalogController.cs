add6228c961199cb200d8641a690203ac8b9feb8
smrutihs <smruti.work@outlook.com>
2013-07-25 23:04:32 -0700

    resequenced the namespaces used

--- HSG.Web/Controllers/CatalogController.cs
+++ HSG.Web/Controllers/CatalogController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using HSG.Library;
 using log4net;
+using HSG.Library;
+using System.Web.Mvc;
 using HSG.Web.CatalogServiceReference;
 
 namespace HSG.Web.Controllers
